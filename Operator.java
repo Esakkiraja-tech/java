6c8d05937a3ecfd63639238b54e6cc0ee5f1f4b6
Esakkiraja-tech/java <rajarajauk442@gmail.com>
2026-06-23 10:07:51 +0530

    first commit

---
+++ Operator.java
@@ -0,0 +1,9 @@
+class Operator {
+    public static void main(String[] args) {
+        int i=7;
+        if(i<10){
+        }
+             System.out.println("i is less than 10");
+    }
+    
+}
---
+++ class Hello{.java
@@ -0,0 +1,5 @@
+class Hello{
+    public static void main(string[]argsd){
+        system.out.println("Hello world");
+    }
+}
